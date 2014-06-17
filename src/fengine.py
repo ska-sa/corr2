11cd41985345aba77f4b88f256a248626172d109
andrew <andrew@ska.ac.za>
2014-06-17 23:07:03 +0200

    basic fengine, xengine
    These engines are decoupled from engines and cannot have instances
    made of them

--- src/fengine.py
+++ src/fengine.py
@@ -7,19 +7,48 @@
 import logging
 LOGGER = logging.getLogger(__name__)
 
-from corr2.engine import Engine
+from misc import log_runtime_error, log_not_implemented_error 
 
-import iniparse
-
-class Fengine(Engine):
-    ''' An F-engine  
+class Fengine():
+    ''' An engine that channelises data from an antenna input.
     '''
 
-    def __init__(self, parent, engine_id, config_file=None, descriptor='f-engine'):
+    def __init__(self, ant_id):
         ''' Constructor 
+        @param ant_id: antenna input identity of data being processed
+        @param descriptor: section in config to locate engine specific info at
         '''
-        Engine.__init__(self, parent, engine_id, descriptor)
+        msg = 'ant_id must be a positive integer'
+        if not isinstance(ant_id, int):
+            log_runtime_error(LOGGER, msg)
 
-    #TODO generic fengine stuff
+        if not ant_id >= 0:
+            log_runtime_error(LOGGER, msg)
+
+        self.ant_id = ant_id
+
+        if not hasattr(self, 'config_portal'):
+            log_runtime_error(LOGGER, 'Fengines can only be ancestors in companion with engines with config_portals')
+        
+        self._get_fengine_config()  
+
+    def _get_fengine_config(self):
+        ''' Get configuration info for this fengine
+        '''
+        self.config['n_chans'] = self.config_portal.get_int(['%s' %self.descriptor, 'n_chans'])
+
+        # delay tracking
+        self.config['min_ld_time'] = self.config_portal.get_float(['delay_tracking', 'min_ld_time'])
+        self.config['network_latency_adjust'] = self.config_portal.get_float(['delay_tracking', 'network_latency_adjust'])
+
+    def set_delay(self, delay=0, delay_delta=0, phase=0, phase_delta=0, load_time=None):
+        ''' Apply delay correction coefficients to polarisation specified from time specified
+        @param delay: delay in samples
+        @param delay_delta: change in delay in samples per ADC sample 
+        @param phase: initial phase offset TODO units
+        @param phase_delta: change in phase TODO units
+        @param load_time: time to load values in ADC samples since epoch. If None load immediately
+        '''
+        log_not_implemented_error(LOGGER, 'fengine.set_delay not implemented')
 
 # end
--- src/xengine.py
+++ src/xengine.py
@@ -7,15 +7,21 @@
 import logging
 LOGGER = logging.getLogger(__name__)
 
-from corr2.engine import Engine
+from corr2.misc import log_runtime_error
 
-class Xengine(Engine):
+class Xengine():
     '''
-    An X-engine, regardless of where it is located.
+    An X-engine, regardless of where it is located. 
+    Xengines cross multiply a subset of channels from a number of fengines and accumulate the result
     '''
-    def __init__(self, parent):
+    def __init__(self, f_offset):
         '''Constructor.
+        @param f_index: the index of the band of frequencies this engine processes
         '''
-        Engine.__init__(self, parent, 'x-engine')
-        LOGGER.info('Xengine created @ %s', str(self.parent))
+        if not isinstance(f_offset, int):
+            log_runtime_error(LOGGER, "f_index must be an integer")        
+
+        #TODO more checking of f_offset
+        self.f_offset = f_offset
+
 # end
