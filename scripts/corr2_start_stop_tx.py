3d6567aee184b1c4c5a4708f3661bca4e498d3b3
Paul Prozesky <paulp@ska.ac.za>
2014-09-23 13:19:43 +0200

    Updated with board classes, fixes.

--- scripts/corr2_start_stop_tx.py
+++ scripts/corr2_start_stop_tx.py
@@ -17,6 +17,8 @@ parser = argparse.ArgumentParser(description='Start or stop 10gbe transmission o
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument(dest='hosts', type=str, action='store',
                     help='comma-delimited list of hosts, or a corr2 config file')
+parser.add_argument('--class', dest='hclass', action='store', default='',
+                    help='start/stop a class: fengine or xengine')
 parser.add_argument('--start', dest='start', action='store_true', default=False,
                     help='start TX')
 parser.add_argument('--stop', dest='stop', action='store_true', default=False,
@@ -43,13 +45,22 @@ else:
     else:
         HOSTCLASS = dcp_fpga.DcpFpga
 
-    hosts = utils.parse_hosts(args.hosts)
+    # are we doing it by class?
+    if args.hclass != '':
+        if args.hclass == 'fengine':
+            hosts = utils.parse_hosts(args.hosts, section='fengine')
+        elif args.hclass == 'xengine':
+            hosts = utils.parse_hosts(args.hosts, section='xengine')
+        else:
+            raise RuntimeError('No such host class: %s' % args.hclass)
+    else:
+        hosts = utils.parse_hosts(args.hosts)
     if len(hosts) == 0:
         raise RuntimeError('No good carrying on without hosts.')
 
     # create the devices and start/stop tx
     fpgas = fpgautils.threaded_create_fpgas_from_hosts(HOSTCLASS, hosts)
-    fpgautils.threaded_fpga_function(fpgas, 'get_system_information')
+    fpgautils.threaded_fpga_function(fpgas, 10, 'get_system_information')
 
 
     def start_stop(stopstart):
@@ -57,17 +68,17 @@ else:
         if fpgas[0].registers.names().count('control') > 0:
             if fpgas[0].registers.control.field_names().count('gbe_out_en') > 0:
                 print '\tProduction control registers found.'
-                fpgautils.threaded_fpga_operation(fpgas, lambda fpga: fpga.registers.control.write(gbe_out_en=stopstart))
+                fpgautils.threaded_fpga_operation(fpgas, 10, lambda fpga: fpga.registers.control.write(gbe_out_en=stopstart))
             elif fpgas[0].registers.control.field_names().count('gbe_txen') > 0:
                 print '\tSim f-engine control registers found.'
-                fpgautils.threaded_fpga_operation(fpgas, lambda fpga: fpga.registers.control.write(gbe_txen=stopstart))
+                fpgautils.threaded_fpga_operation(fpgas, 10, lambda fpga: fpga.registers.control.write(gbe_txen=stopstart))
             elif fpgas[0].registers.control.field_names().count('comms_en') > 0:
                 print '\tOld-style fengine control registers found.'
-                fpgautils.threaded_fpga_operation(fpgas, lambda fpga: fpga.registers.control.write(comms_en=stopstart))
+                fpgautils.threaded_fpga_operation(fpgas, 10, lambda fpga: fpga.registers.control.write(comms_en=stopstart))
         elif fpgas[0].registers.names().count('ctrl') > 0:
             if fpgas[0].registers.ctrl.field_names().count('comms_en') > 0:
                 print '\tSome control registers found?'
-                print fpgautils.threaded_fpga_operation(fpgas, lambda fpga: fpga.registers.ctrl.write(comms_en=stopstart))
+                print fpgautils.threaded_fpga_operation(fpgas, 10, lambda fpga: fpga.registers.ctrl.write(comms_en=stopstart))
         else:
             print '\tCould not find the correct registers to control TX'
 
@@ -79,6 +90,6 @@ else:
     if args.start:
         start_stop(True)
 
-    fpgautils.threaded_fpga_function(fpgas, 'disconnect')
+    fpgautils.threaded_fpga_function(fpgas, 10, 'disconnect')
 
 # end
