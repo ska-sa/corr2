befcb1bfd9357bab5bdc34902fa2f1d0ecc5b4e0
Paul Prozesky <paulp@ska.ac.za>
2015-03-12 15:30:59 +0200

    Fixed fhost.get_fft_shift()

--- src/fhost_fpga.py
+++ src/fhost_fpga.py
@@ -231,7 +231,7 @@ class FpgaFHost(FpgaHost):
         Get the current FFT shift schedule from the FPGA.
         :return: integer representing the FFT shift schedule for all the FFTs on this engine.
         """
-        return self.host.registers.fft_shift.read()['data']['fft_shift']
+        return self.registers.fft_shift.read()['data']['fft_shift']
 
 '''
     def _get_fengine_fpga_config(self):
