d58785d64457f8992f3b8d22f00c3d75c3995d92
Neilen Marais <nmarais@ska.ac.za>
2015-05-11 17:15:46 +0200

    Initial dsim-host class, seems to be able to programme the D-engine

---
+++ src/dsimhost_fpga.py
@@ -0,0 +1,157 @@
+import logging
+import time
+
+from casperfpga.attribute_container import AttributeContainer
+from casperfpga import tengbe
+
+from corr2.host_fpga import FpgaHost
+
+LOGGER = logging.getLogger(__name__)
+
+def get_prefixed_name(prefix, string):
+    if not string.startswith(prefix):
+        return None
+    if string.startswith(prefix+'_'):
+        return string[len(prefix)+1:]
+    else:
+        return string[len(prefix):]
+
+class Source(object):
+    def __init__(self, register, name):
+        self.register = register
+        self.name = name
+
+class SineSource(Source):
+    def set(self, scale=None, frequency=None):
+        # TODO work in 'human' units such as Hz or Volts?
+        self.register.write(scale=scale, frequency=frequency)
+
+class NoiseSource(Source):
+    def set(self, scale=None, frequency=None):
+        # TODO work in 'human' units such W / Hz?
+        self.register.write(scale=scale, frequency=frequency)
+
+class Output(object):
+    def __init__(self, name, scale_register, control_register):
+        self.name = name
+        self.scale_register = scale_register
+        self.control_register = control_register
+
+    def select_output(self, output_type):
+        tgv_select = 'tvg_select' + self.name
+        if output_type == 'test_vectors':
+            self.control_register.write(**{tgv_select: 0})
+        elif output_type == 'signal':
+            self.control_register.write(**{tgv_select: 1})
+        else:
+            raise ValueError(
+                'Valid output_type values: "test_vectors" and "signal"')
+
+    def scale_output(self, scale):
+        # TODO normalized scale to take values between 0 and 1?
+        self.scale_register.write(scale=scale)
+
+class FpgaDsimHost(FpgaHost):
+    def __init__(self, host, katcp_port=7147, boffile=None, connect=True, config=None):
+        if config:
+            if boffile:
+                raise ValueError('Cannot specify "boffile" parameter if config is used')
+            boffile = config['bitstream']
+        self.config = config
+        FpgaHost.__init__(
+            self, host, katcp_port=katcp_port, boffile=boffile, connect=connect)
+        self.boffile = boffile
+        self.sine_sources = AttributeContainer()
+        self.noise_sources = AttributeContainer()
+        self.outputs = AttributeContainer()
+
+    def get_system_information(self, filename=None, fpg_info=None):
+        """Get system information and build D-engine sources"""
+        FpgaHost.get_system_information(self, filename=filename, fpg_info=fpg_info)
+        self.sine_sources.clear()
+        self.noise_sources.clear()
+        self.outputs.clear()
+        for reg in self.registers:
+            sin_name = get_prefixed_name('freq_cwg', reg.name)
+            noise_name = get_prefixed_name('scale_wng', reg.name)
+            output_scale_name = get_prefixed_name('scale_out', reg.name)
+            if sin_name is not None:
+                setattr(self.sine_sources, 'sin_'+ sin_name, SineSource(reg, sin_name))
+            elif noise_name is not None:
+                setattr(self.noise_sources, 'noise_' + noise_name,
+                        NoiseSource(reg, noise_name))
+            elif output_scale_name is not None:
+                setattr(self.outputs, 'out_' + output_scale_name,
+                        Output(output_scale_name, reg, self.registers.control))
+
+    def initialize(self):
+        """Program (if self.boffile is specified) and init Dsim roach"""
+        if not self.is_connected():
+            self.connect()
+        if self.boffile:
+            self._program()
+        else:
+            LOGGER.info('Not programming host {} since no boffile is configured'
+                        .format(self.host))
+        if not self.is_running():
+            raise RuntimeError('D-engine {host} not running'
+                               .format(**self.__dict__))
+        self.get_system_information()
+        self._setup_tengbes()
+        # Set digitizer polarisation IDs, 0 - h, 1 - v
+        self.registers.receptor_id.write(pol0_id=0, pol1_id=1)
+
+        # start the local timer on the test d-engine - mrst, then a fake sync
+        self.registers.control.write(mrst='pulse')
+        self.registers.control.write(msync='pulse')
+
+        # Default to generating test-vectors
+        for output in self.outputs:
+            output.select_output('test_vectors')
+
+    def _program(self):
+        """Program the boffile to fpga and ensure 10GbE's are not transmitting"""
+        LOGGER.info('Programming Dsim roach {host} with file {boffile}'
+                    .format(**self.__dict__))
+        stime = time.time()
+        self.upload_to_ram_and_program(self.boffile)
+        LOGGER.info('Programmed %s in %.2f seconds.' % (
+            self.host, time.time() - stime))
+        # Ensure data is not sent before the tengbe's are configured
+        self.registers.control.write(gbe_txen=False)
+
+    def _setup_tengbes(self):
+        """Set up 10GbE MACs, IPs and destination address/port"""
+        start_ip = self.config['10gbe_start_ip']
+        start_mac = self.config['10gbe_start_mac']
+        port = int(self.config['10gbe_port'])
+        ip_bits = start_ip.split('.')
+        ipbase = int(ip_bits[3])
+        mac_bits = start_mac.split(':')
+        macbase = int(mac_bits[5])
+        num_tengbes = 4         # Harcoded assumption
+        for ctr in range(num_tengbes):
+            mac = '%s:%d' % (':'.join(mac_bits[0:5]), macbase + ctr)
+            ip = '%s.%s.%s.%d' % (ip_bits[0], ip_bits[1], ip_bits[2], ipbase + ctr)
+            self.tengbes['gbe%d' % ctr].setup(mac=mac, ipaddress=ip, port=port)
+        for gbe in self.tengbes:
+            gbe.tap_start(True)
+        # set the destination IP and port for the tx
+        txaddr = start_ip
+        txaddr_bits = txaddr.split('.')
+        txaddr_base = int(txaddr_bits[3])
+        txaddr_prefix = '.'.join(txaddr_bits[0:3])
+        for ctr in range(0, num_tengbes):
+            txip = txaddr_base + ctr
+            LOGGER.info('%s sending to: %s%d port %d' % (
+                self.host, txaddr_prefix, txip, port))
+            self.write_int('gbe_iptx%i' % ctr,
+                           tengbe.str2ip('%s.%d' % (txaddr_prefix, txip)))
+        self.write_int('gbe_porttx', port)
+        self.registers.control.write(gbe_rst=False)
+
+    def enable_data_output(self):
+        """Make the data flow!"""
+        self.registers.control.write(gbe_txen=True)
+        self.registers.control_output.write(load_en_time='pulse')
+
