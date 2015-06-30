de80b22c9090ccf2655c22b605d82801540a42f5
mobrocka <mobrocka@ska.ac.za>
2015-06-30 16:20:25 +0200

    Added ioloop to the new sensor.py

--- src/sensors.py
+++ src/sensors.py
@@ -17,118 +17,140 @@ def _sensor_cb_flru(instr, sensor):
     instr.logger.info('_sensor_cb_flru ran on {}'.format(host_name))
     IOLoop.current().call_later(10, _sensor_cb_flru, instr, sensor)
 
+def _sensor_cb_xlru(instr, sensor):
+    """
+    Sensor call back function for x-engine LRU
+    :param sensor:
+    :return:
+    """
+    host_name = sensor.name.split('_')[2]
+    result = False
+    for _xhost in instr.xhosts:
+        if _xhost.host == host_name:
+            result = _xhost.host_okay()
+    sensor.set(time.time(), Sensor.NOMINAL if result else Sensor.ERROR, result)
+    instr.logger.info('_sensor_cb_xlru ran on {}'.format(host_name))
+    IOLoop.current().call_later(10, _sensor_cb_xlru, instr, sensor)
 
-# def _sensor_cb_xlru(self, sensor):
-#     """
-#     Sensor call back function for x-engine LRU
-#     :param sensor:
-#     :return:
-#     """
-#     host_name = sensor.name.split('_')[2]
-#     result = self.xhosts[host_name].host_okay()
-#     sensor.set(time.time(), Sensor.NOMINAL if result else Sensor.ERROR, result)
-#     self.logger.info('_sensor_cb_xlru ran on %s' % (host_name))
-#     Timer(10, self._sensor_cb_xlru, sensor).start()
-#
-# def _sensor_feng_tx(self, sensor):
-#     """
-#     f-engine tx counters
-#     :param sensor:
-#     :return: rv
-#     """
-#     host_name = sensor.name.split('_')[2]
-#     result = self.fhosts[host_name].tengbes.tx_okay()
-#     sensor.set(time.time(), Sensor.NOMINAL if result else Sensor.ERROR, result)
-#     self.logger.info('_sensor_feng_tx ran on %s' % (host_name))
-#     Timer(10, self._sensor_feng_tx, sensor).start()
-#
-# def _sensor_feng_rx(self, sensor):
-#     """
-#     f-engine rx counters
-#     :param sensor:
-#     :return: true/false
-#     """
-#     host_name = sensor.name.split('_')[2]
-#     result = self.fhosts[host_name].check_rx_reorder()
-#     sensor.set(time.time(), Sensor.NOMINAL if result else Sensor.ERROR, result)
-#     self.logger.info('_sensor_feng_rx ran on %s' % (host_name))
-#     Timer(10, self._sensor_feng_rx, sensor).start()
-#
-# def _sensor_xeng_tx(self, sensor):
-#     """
-#     x-engine tx counters
-#     :param sensor:
-#     :return:
-#     """
-#     host_name = sensor.name.split('_')[2]
-#     result = self.xhosts[host_name].tengbes.tx_okay()
-#     sensor.set(time.time(), Sensor.NOMINAL if result else Sensor.ERROR, result)
-#     self.logger.info('_sensor_xeng_tx ran on %s' % (host_name))
-#     Timer(10, self._sensor_xeng_tx, sensor).start()
-#
-# def _sensor_xeng_rx(self, sensor):
-#     """
-#     x-engine rx counters
-#     :param sensor:
-#     :return:
-#     """
-#     host_name = sensor.name.split('_')[2]
-#     result = self.xhosts[host_name].check_rx_reorder()
-#     sensor.set(time.time(), Sensor.NOMINAL if result else Sensor.ERROR, result)
-#     self.logger.info('_sensor_xeng_rx ran on %s' % (host_name))
-#     Timer(10, self._sensor_xeng_rx, sensor).start()
-#
-# def _sensor_feng_phy(self, sensor):
-#     """
-#     f-engine PHY counters
-#     :param sensor:
-#     :return:
-#     """
-#     host_name = sensor.name.split('_')[2]
-#     result = self.fhosts[host_name].check_phy_counter()
-#     sensor.set(time.time(), Sensor.NOMINAL if result else Sensor.ERROR, result)
-#     self.logger.info('_sensor_feng_phy ran on %s' % host_name)
-#     Timer(10, self._sensor_feng_phy, sensor).start()
-#     return
-#
-# def _sensor_xeng_phy(self, sensor):
-#     """
-#     x-engine PHY counters
-#     :param sensor:
-#     :return:
-#     """
-#     host_name = sensor.name.split('_')[2]
-#     result = self.xhosts[host_name].check_phy_counter()
-#     sensor.set(time.time(), Sensor.NOMINAL if result else Sensor.ERROR, result)
-#     self.logger.info('_sensor_xeng_phy ran on %s' % host_name)
-#     Timer(10, self._sensor_xeng_phy, sensor).start()
-#     return
-#
-# def _xeng_qdr_okay(self, sensor):
-#     """
-#     x-engine QDR check
-#     :param sensor:
-#     :return:
-#     """
-#     host_name = sensor.name.split('_')[2]
-#     result = self.xhosts[host_name].qdr_okay()
-#     sensor.set(time.time(), Sensor.NOMINAL if result else Sensor.ERROR, result)
-#     self.logger.info('_xeng_qdr_okay ran on %s' % host_name)
-#     Timer(10, self._xeng_qdr_okay, sensor).start()
-#     return
-#
-# def _feng_qdr_okay(self, sensor):
-#     """
-#     f-engine QDR check
-#     :param sensor:
-#     :return:
-#     """
-#     host_name = sensor.name.split('_')[2]
-#     result = self.fhosts[host_name].qdr_okay()
-#     sensor.set(time.time(), Sensor.NOMINAL if result else Sensor.ERROR, result)
-#     self.logger.info('_feng_qdr_okay ran on %s' % host_name)
-#     IOLoop.instance().call_later(self._feng_qdr_okay, 10)
-#     return
+def _sensor_feng_tx(instr, sensor):
+    """
+    f-engine tx counters
+    :param sensor:
+    :return: rv
+    """
+    host_name = sensor.name.split('_')[2]
+    result = False
+    for _fhost in instr.fhosts:
+        if _fhost.host == host_name:
+            result = _fhost.tengbes.tx_okay()
+    sensor.set(time.time(), Sensor.NOMINAL if result else Sensor.ERROR, result)
+    instr.logger.info('_sensor_feng_tx ran on %s' % (host_name))
+    IOLoop.current().call_later(10, _sensor_feng_tx, instr, sensor)
+
+def _sensor_feng_rx(instr, sensor):
+    """
+    f-engine rx counters
+    :param sensor:
+    :return: true/false
+    """
+    host_name = sensor.name.split('_')[2]
+    result = False
+    for _fhost in instr.fhosts:
+        if _fhost.host == host_name:
+            result = _fhost.check_rx_reorder()
+    sensor.set(time.time(), Sensor.NOMINAL if result else Sensor.ERROR, result)
+    instr.logger.info('_sensor_feng_rx ran on %s' % (host_name))
+    IOLoop.current().call_later(10, _sensor_feng_rx, instr, sensor)
+
+def _sensor_xeng_tx(instr, sensor):
+    """
+    x-engine tx counters
+    :param sensor:
+    :return:
+    """
+    host_name = sensor.name.split('_')[2]
+    result = False
+    for _xhost in instr.xhosts:
+        if _xhost.host == host_name:
+            result = _xhost.tengbes.tx_okay()
+    sensor.set(time.time(), Sensor.NOMINAL if result else Sensor.ERROR, result)
+    instr.logger.info('_sensor_xeng_tx ran on %s' % (host_name))
+    IOLoop.current().call_later(10, _sensor_xeng_tx, instr, sensor)
+
+def _sensor_xeng_rx(instr, sensor):
+    """
+    x-engine rx counters
+    :param sensor:
+    :return:
+    """
+    host_name = sensor.name.split('_')[2]
+    result = False
+    for _xhost in instr.xhosts:
+        if _xhost.host == host_name:
+            result = _xhost.check_rx_reorder()
+    sensor.set(time.time(), Sensor.NOMINAL if result else Sensor.ERROR, result)
+    instr.logger.info('_sensor_xeng_rx ran on %s' % (host_name))
+    IOLoop.current().call_later(10, _sensor_xeng_rx, instr, sensor)
+
+def _sensor_feng_phy(instr, sensor):
+    """
+    f-engine PHY counters
+    :param sensor:
+    :return:
+    """
+    host_name = sensor.name.split('_')[2]
+    result = False
+    for _fhost in instr.fhosts:
+        if _fhost.host == host_name:
+            result = _fhost.check_phy_counter()
+    sensor.set(time.time(), Sensor.NOMINAL if result else Sensor.ERROR, result)
+    instr.logger.info('_sensor_feng_phy ran on %s' % host_name)
+    IOLoop.current().call_later(10, _sensor_feng_phy, instr, sensor)
+
+def _sensor_xeng_phy(instr, sensor):
+    """
+    x-engine PHY counters
+    :param sensor:
+    :return:
+    """
+    host_name = sensor.name.split('_')[2]
+    result = False
+    for _xhost in instr.xhosts:
+        if _xhost.host == host_name:
+            result = _xhost.check_phy_counter()
+    sensor.set(time.time(), Sensor.NOMINAL if result else Sensor.ERROR, result)
+    instr.logger.info('_sensor_xeng_phy ran on %s' % host_name)
+    IOLoop.current().call_later(10, _sensor_xeng_phy, instr, sensor)
+
+def _xeng_qdr_okay(instr, sensor):
+    """
+    x-engine QDR check
+    :param sensor:
+    :return:
+    """
+    host_name = sensor.name.split('_')[2]
+    result = False
+    for _xhost in instr.xhosts:
+        if _xhost.host == host_name:
+            result = _xhost.qdr_okay()
+    sensor.set(time.time(), Sensor.NOMINAL if result else Sensor.ERROR, result)
+    instr.logger.info('_xeng_qdr_okay ran on %s' % host_name)
+    IOLoop.current().call_later(10, _xeng_qdr_okay, instr, sensor)
+
+def _feng_qdr_okay(instr, sensor):
+    """
+    f-engine QDR check
+    :param sensor:
+    :return:
+    """
+    host_name = sensor.name.split('_')[2]
+    result = False
+    for _fhost in instr.fhosts:
+        if _fhost.host == host_name:
+            result = _fhost.qdr_okay()
+    sensor.set(time.time(), Sensor.NOMINAL if result else Sensor.ERROR, result)
+    instr.logger.info('_feng_qdr_okay ran on %s' % host_name)
+    IOLoop.current().call_later(10, _feng_qdr_okay, instr, sensor)
 
 def setup_sensors(instrument, katcp_server):
     """
@@ -156,27 +178,15 @@ def setup_sensors(instrument, katcp_server):
         instrument._sensors[sensor.name] = sensor
         ioloop.add_callback(_sensor_cb_flru, instrument, sensor)
 
-#     # x-engine lru
-# =======
-#         sensor.set(time.time(),
-#                    Sensor.NOMINAL if okay_res[_f.host] else Sensor.ERROR,
-#                    okay_res[_f.host])
-#         self._sensors[sensor.name] = sensor
-#     okay_res = THREADED_FPGA_FUNC(self.xhosts, timeout=5, target_function='host_okay')
-# >>>>>>> devel
-#     for _x in self.xhosts:
-#         sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='xeng_lru_%s' % _x.host,
-#                         description='X-engine %s LRU okay' % _x.host,
-#                         default=True)
-# <<<<<<< HEAD
-#         ioloop.add_callback(self._sensor_cb_xlru, sensor)  # call back function
-# =======
-#         sensor.set(time.time(),
-#                    Sensor.NOMINAL if okay_res[_x.host] else Sensor.ERROR,
-#                    okay_res[_x.host])
-# >>>>>>> devel
-#         self._sensors[sensor.name] = sensor
-#
+    # x-engine lru
+    for _x in self.xhosts:
+        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='xeng_lru_%s' % _x.host,
+                        description='X-engine %s LRU okay' % _x.host,
+                        default=True)
+        katcp_server.add_sensor(sensor)
+        instrument._sensors[sensor.name] = sensor
+        ioloop.add_callback(_sensor_cb_xlru, instrument, sensor)
+
 #     # self._sensors = {'time': Sensor(sensor_type=Sensor.FLOAT, name='time_sensor', description='The time.',
 #     #                                units='s', params=[-(2**64), (2**64)-1], default=-1),
 #     #                 'test': Sensor(sensor_type=Sensor.INTEGER, name='test_sensor',
@@ -186,71 +196,75 @@ def setup_sensors(instrument, katcp_server):
 #     #                                     description='The meta dest string',
 #     #                                     units='', default=str(self.meta_destination))
 #     #                 }
-#
-#     # f-engine tx counters
-#     for _f in self.fhosts:
-#         sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='feng_tx_%s' % _f.host,
-#                         description='F-engine TX okay - counters incrementing' % _f.host,
-#                         default=True)
-#         self._sensor_feng_rx(sensor)
-#         self._sensors[sensor.name] = sensor
-#
-#     # f-engine rx counters
-#     for _f in self.fhosts:
-#         sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='feng_rx_%s' % _f.host,
-#                         description='F-engine %s RX okay - counters incrementing' % _f.host,
-#                         default=True)
-#         self._sensor_feng_rx(sensor)
-#         self._sensors[sensor.name] = sensor
-#
-#     # x-engine tx counters
-#     sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='xeng_tx',
-#                     description='X-engine TX okay - counters incrementing',
-#                     default=True)
-#     self._sensor_xeng_rx(sensor)
-#     self._sensors[sensor.name] = sensor
-#
-#     # x-engine rx counters
-#     for _x in self.xhosts:
-#         sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='xeng_rx_%s' % _x.host,
-#                         description='X-engine RX okay - counters incrementing' % _x.host,
-#                         default=True)
-#         self._sensor_xeng_rx(sensor)
-#         self._sensors[sensor.name] = sensor
-#
-#     # # x-engine QDR errors
-#     for _x in self.xhosts:
-#         sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='xeng_qdr_%s' % _x.host,
-#                         description='X-engine QDR okay' % _x.host,
-#                         default=True)
-#         self._xeng_qdr_okay(sensor)
-#         self._sensors[sensor.name] = sensor
-#
-#     # # f-engine QDR errors
-#     for _f in self.fhosts:
-#         sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='feng_qdr_%s' % _f.host,
-#                         description='F-engine QDR okay' % _f.host,
-#                         default=True)
-#         self._feng_qdr_okay(sensor)
-#         self._sensors[sensor.name] = sensor
-#
-#     # # x-engine PHY counters
-#     for _x in self.xhosts:
-#         sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='xeng_PHY_%s' % _x.host,
-#                         description='X-engine PHY okay' % _x.host,
-#                         default=True)
-#         self._sensor_xeng_phy(sensor)
-#         self._sensors[sensor.name] = sensor
-#
-#     # # f-engine PHY counters
-#     for _f in self.fhosts:
-#         sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='feng_PHY_%s' % _f.host,
-#                         description='F-engine PHY okay' % _f.host,
-#                         default=True)
-#         self._sensor_feng_phy(sensor)
-#         self._sensors[sensor.name] = sensor
-#
-#     for val in self._sensors.values():
-#         katcp_server.add_sensor(val)
-#     Timer(self.sensor_poll_time, self._update_sensors).start()
-#
+
+    # f-engine tx counters
+    for _f in instrument.fhosts:
+        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='feng_tx_%s' % _f.host,
+                        description='F-engine TX okay - counters incrementing' % _f.host,
+                        default=True)
+        katcp_server.add_sensor(sensor)
+        instrument._sensors[sensor.name] = sensor
+        ioloop.add_callback(_sensor_feng_tx, instrument, sensor)
+
+    # f-engine rx counters
+    for _f in instrument.fhosts:
+        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='feng_rx_%s' % _f.host,
+                        description='F-engine %s RX okay - counters incrementing' % _f.host,
+                        default=True)
+        katcp_server.add_sensor(sensor)
+        instrument._sensors[sensor.name] = sensor
+        ioloop.add_callback(_sensor_feng_rx, instrument, sensor)
+
+    # x-engine tx counters
+    for _x in instrument.xhosts:
+        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='xeng_tx_%s' % _x.host,
+                        description='X-engine TX okay - counters incrementing' % _x.host,
+                        default=True)
+        katcp_server.add_sensor(sensor)
+        instrument._sensors[sensor.name] = sensor
+        ioloop.add_callback(_sensor_xeng_tx, instrument, sensor)
+
+    # x-engine rx counters
+    for _x in instrument.xhosts:
+        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='xeng_rx_%s' % _x.host,
+                        description='X-engine RX okay - counters incrementing' % _x.host,
+                        default=True)
+        katcp_server.add_sensor(sensor)
+        instrument._sensors[sensor.name] = sensor
+        ioloop.add_callback(_sensor_xeng_rx, instrument, sensor)
+
+    # x-engine QDR errors
+    for _x in instrument.xhosts:
+        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='xeng_qdr_%s' % _x.host,
+                        description='X-engine QDR okay' % _x.host,
+                        default=True)
+        katcp_server.add_sensor(sensor)
+        instrument._sensors[sensor.name] = sensor
+        ioloop.add_callback(_xeng_qdr_okay, instrument, sensor)
+
+    # f-engine QDR errors
+    for _f in instrument.fhosts:
+        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='feng_qdr_%s' % _f.host,
+                        description='F-engine QDR okay' % _f.host,
+                        default=True)
+        katcp_server.add_sensor(sensor)
+        instrument._sensors[sensor.name] = sensor
+        ioloop.add_callback(_feng_qdr_okay, instrument, sensor)
+
+    # x-engine PHY counters
+    for _x in instrument.xhosts:
+        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='xeng_PHY_%s' % _x.host,
+                        description='X-engine PHY okay' % _x.host,
+                        default=True)
+        katcp_server.add_sensor(sensor)
+        instrument._sensors[sensor.name] = sensor
+        ioloop.add_callback(_sensor_xeng_phy, instrument, sensor)
+
+    # f-engine PHY counters
+    for _f in instrument.fhosts:
+        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='feng_PHY_%s' % _f.host,
+                        description='F-engine PHY okay' % _f.host,
+                        default=True)
+        katcp_server.add_sensor(sensor)
+        instrument._sensors[sensor.name] = sensor
+        ioloop.add_callback(_sensor_feng_phy, instrument, sensor)
