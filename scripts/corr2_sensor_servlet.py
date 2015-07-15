048d8a2c0172cc9e54426a83fd2a7045924ef095
mobrocka <mobrocka@ska.ac.za>
2015-07-15 17:47:20 +0200

    Added tornado coroutine to the sensors.py and modified 'main' in sensor servlet

--- scripts/corr2_sensor_servlet.py
+++ scripts/corr2_sensor_servlet.py
@@ -5,11 +5,15 @@ __author__ = 'paulp'
 import logging
 import sys
 import argparse
+import os
 import Queue
 
 import katcp
+
+from tornado.ioloop import IOLoop
+
 from katcp.kattypes import request, return_reply, Bool
-from corr2 import sensors
+from corr2 import sensors, fxcorrelator
 
 class KatcpLogFormatter(logging.Formatter):
     def format(self, record):
@@ -51,6 +55,7 @@ class Corr2SensorServer(katcp.DeviceServer):
 
     def __init__(self, *args, **kwargs):
         super(Corr2SensorServer, self).__init__(*args, **kwargs)
+        self.set_concurrency_options(thread_safe=False, handler_thread=False)
         self.instrument = None
 
     def setup_sensors(self):
@@ -60,27 +65,19 @@ class Corr2SensorServer(katcp.DeviceServer):
         """
         pass
 
-    @request(Bool(default=True))
-    @return_reply()
-    def request_initialise(self):
+    def initialise(self, instrument):
         """
         Setup and start sensors
         :param:
         :return:
+
         """
-        try:
-            self.instrument.initialise(program=False, tvg=False)
-            sensors.setup_sensors(instrument=self.instrument, katcp_server=self)
-            return 'ok',
-        except Exception as e:
-            localexc = e
-            pass
-        logging.error('create threw Exception: %s' % localexc.message)
-        return 'fail', 'create threw Exception: %s' % localexc.message
+        self.instrument = instrument
+        self.instrument.initialise(program=False, tvg=False)
+        sensors.setup_sensors(instrument=self.instrument, katcp_server=self)
 
 if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(description='Start a corr2 instrument server.',
+    parser = argparse.ArgumentParser(description='Start a corr2 sensor server.',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-p', '--port', dest='port', action='store', default=1235, type=int,
                         help='bind to this port to receive KATCP messages')
@@ -88,15 +85,14 @@ if __name__ == '__main__':
                         help='log level to set')
     parser.add_argument('--log_format_katcp', dest='lfm', action='store_true', default=False,
                         help='format log messsages for katcp')
-    # parser.add_argument('-c', '--config', dest='configfile', action='store',
-    #                     default='', type=str,
-    #                     help='config file location')
     args = parser.parse_args()
 
     try:
         log_level = eval('logging.%s' % args.loglevel)
     except:
         raise RuntimeError('Received nonsensical log level %s' % args.loglevel)
+
+    ioloop = IOLoop.current()
 
     # set up the logger
     root_logger = logging.getLogger()
@@ -113,33 +109,16 @@ if __name__ == '__main__':
         root_logger.addHandler(console_handler)
         use_katcp_logging = False
 
-    print 'Server listening on port %d, ' % args.port,
-    queue = Queue.Queue()
-    server = Corr2Server('127.0.0.1', args.port)
-    if use_katcp_logging:
-        katcp_emit_handler = KatcpLogEmitHandler(server, stream=sys.stdout)
-        katcp_emit_handler.setLevel(log_level)
-        root_logger.addHandler(katcp_emit_handler)
-    server.set_restart_queue(queue)
-    server.start()
-    print 'started. Running somewhere in the ether... exit however you see fit.'
-
     try:
-        while True:
-            try:
-                device = queue.get(timeout=0.5)
-            except Queue.Empty:
-                device = None
-            if device is not None:
-                print 'Stopping...'
-                device.stop()
-                device.join()
-                print 'Restarting...'
-                device.start()
-                print 'Started.'
-    except KeyboardInterrupt:
-        print 'Shutting down...'
-        server.stop()
-        server.join()
-
+        print 'Sensor Server listening on port %d, ' % args.port
+        sensor_server = Corr2SensorServer('127.0.0.1', args.port)
+        sensor_server.set_ioloop(ioloop)
+        ioloop.add_callback(sensor_server.start)
+        config_file = os.environ['CORR2INI']
+        instrument = fxcorrelator.FxCorrelator('RTS correlator', config_source=config_file)
+        ioloop.add_callback(sensor_server.initialise, instrument)
+        ioloop.start()
+        print 'Sensor Server finished ini'
+    except ValueError:
+        print 'Failed'
 # end
