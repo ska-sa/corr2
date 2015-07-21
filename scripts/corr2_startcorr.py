a457c69893402880bfb40478168c88094c1e7be6
Paul Prozesky <paulp@ska.ac.za>
2015-07-21 11:27:51 +0200

    Fixed logging, added debugging to ipython

--- scripts/corr2_startcorr.py
+++ scripts/corr2_startcorr.py
@@ -13,7 +13,7 @@ parser.add_argument('--noprogram', dest='noprogram', action='store_true', defaul
                     help='do NOT program the FPGAs, and everything that goes with that')
 parser.add_argument('--noqdrcal', dest='no_qdr_cal', action='store_true', default=False,
                     help='do NOT calibrate the QDRs')
-parser.add_argument('--loglevel', dest='log_level', action='store', default='',
+parser.add_argument('--loglevel', dest='log_level', action='store', default='INFO',
                     help='log level to use, default None, options INFO, DEBUG, ERROR')
 args = parser.parse_args()
 
@@ -28,7 +28,14 @@ if args.log_level != '':
 if 'CORR2INI' in os.environ.keys() and args.config == '':
     args.config = os.environ['CORR2INI']
 
-c = fxcorrelator.FxCorrelator('rts correlator', config_source=args.config)
-c.initialise(program=not args.noprogram, qdr_cal=not args.no_qdr_cal)
+c = fxcorrelator.FxCorrelator('rts correlator',
+                              config_source=args.config,
+                              log_level=eval('logging.%s' % log_level))
+try:
+    c.initialise(program=not args.noprogram, qdr_cal=not args.no_qdr_cal)
+except Exception as e:
+    print e
+	import IPython
+	IPython.embed()
 
 # end
