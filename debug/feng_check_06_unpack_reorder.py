1bffea1a0458f88859331ce837f5974ff59a386e
Paul Prozesky <paulp@ska.ac.za>
2015-07-21 11:28:21 +0200

    Updated debug scripts

--- debug/feng_check_06_unpack_reorder.py
+++ debug/feng_check_06_unpack_reorder.py
@@ -62,24 +62,14 @@ hosts = utils.parse_hosts(args.hosts, section='fengine')
 if len(hosts) == 0:
     raise RuntimeError('No good carrying on without hosts.')
 
-
 def get_fpga_data(fpga):
-    data = {}
-    for pol in [0, 1]:
-        reord_errors = fpga.registers['updebug_reord_err%i' % pol].read()['data']
-        # data['re%i_cnt' % pol] = reord_errors['cnt']
-        # data['re%i_time' % pol] = reord_errors['time']
-        # data['re%i_tstep' % pol] = reord_errors['timestep']
-        data['timerror%i' % pol] = reord_errors['timestep']
-    valid_cnt = fpga.registers.updebug_validcnt.read()['data']
-    data['valid_arb'] = valid_cnt['arb']
-    data['valid_reord'] = valid_cnt['reord']
-    data['mcnt_relock'] = fpga.registers.mcnt_relock.read()['data']['reg']
-    temp = fpga.registers.pfb_of.read()['data']
-    data['pfb_of0'] = temp['of0']
-    data['pfb_of1'] = temp['of1']
+    data = fpga.registers.reorder_ctrs.read()['data']
+    spead_data = fpga.registers.spead_ctrs.read()['data']
+    spead_pkts = 0
+    for ctr in range(0, 4):
+        spead_pkts += spead_data['rx_cnt%i' % ctr]
+    data['spd_rx'] = spead_pkts
     return data
-
 
 def signal_handler(sig, frame):
     print sig, frame
@@ -98,7 +88,7 @@ max_hostname = -1
 for fpga_ in fpgas:
     max_hostname = max(len(fpga_.host), max_hostname)
     freg_error = False
-    for necreg in ['updebug_reord_err0', 'updebug_reord_err1', 'updebug_validcnt', 'mcnt_relock', 'pfb_of']:
+    for necreg in ['reorder_ctrs']:
         if necreg not in fpga_.registers.names():
             freg_error = True
             continue
@@ -109,17 +99,11 @@ if len(registers_missing) > 0:
     print 'The following hosts are missing necessary registers. Bailing.'
     print registers_missing
     fpgautils.threaded_fpga_function(fpgas, 10, 'disconnect')
-    raise RuntimeError
+    raise RuntimeError('Some FPGAs were missing necessary registers.')
 
 if args.rstcnt:
-    if 'unpack_cnt_rst' in fpgas[0].registers.control.field_names():
-        fpgautils.threaded_fpga_operation(fpgas, 10,
-                                          lambda fpga_: fpga_.registers.control.write(cnt_rst='pulse',
-                                                                                      unpack_cnt_rst='pulse'))
-    else:
-        fpgautils.threaded_fpga_operation(fpgas, 10,
-                                          lambda fpga_: fpga_.registers.control.write(cnt_rst='pulse',
-                                                                                      up_cnt_rst='pulse'))
+    fpgautils.threaded_fpga_operation(fpgas, 10,
+                                      lambda fpga_: fpga_.registers.control.write(cnt_rst='pulse',))
 
 fpga_data = get_fpga_data(fpgas[0])
 reg_names = fpga_data.keys()
@@ -141,27 +125,30 @@ try:
             scroller.draw_screen()
         if time.time() > last_refresh + args.polltime:
             scroller.clear_buffer()
-            scroller.add_line('Polling %i fengine%s every %s - %is elapsed.' %
-                (len(fpgas), '' if len(fpgas) == 1 else 's',
+            scroller.add_line('Polling %i fengine%s every %s - %is elapsed.' % (
+                len(fpgas), '' if len(fpgas) == 1 else 's',
                 'second' if args.polltime == 1 else ('%i seconds' % args.polltime),
                 time.time() - STARTTIME), 0, 0, absolute=True)
             start_pos = max_hostname + 3
-            pos_increment = 20
+            pos_increment = 16
             scroller.add_line('Host', 0, 1, absolute=True)
             for reg in reg_names:
-                scroller.add_line(new_line=reg.rjust(13), xpos=start_pos, ypos=1, absolute=True)
+                scroller.add_line(new_line=reg.rjust(13), xpos=start_pos,
+                                  ypos=1, absolute=True)
                 start_pos += pos_increment
             scroller.set_ypos(newpos=2)
             scroller.set_ylimits(ymin=2)
-            all_fpga_data = fpgautils.threaded_fpga_operation(fpgas, 10, get_fpga_data)
+            all_fpga_data = fpgautils.threaded_fpga_operation(fpgas, 10,
+                                                              get_fpga_data)
             for ctr, ffpga in enumerate(fpgas):
                 fpga_data = all_fpga_data[ffpga.host]
                 scroller.add_line(ffpga.host)
                 start_pos = max_hostname + 3
-                pos_increment = 20
                 for reg in reg_names:
                     regval = '%13d' % fpga_data[reg]
-                    scroller.add_line(regval, start_pos, scroller.get_current_line() - 1) # all on the same line
+                    # all on the same line
+                    scroller.add_line(regval, start_pos,
+                                      scroller.get_current_line() - 1)
                     start_pos += pos_increment
             scroller.draw_screen()
             last_refresh = time.time()
