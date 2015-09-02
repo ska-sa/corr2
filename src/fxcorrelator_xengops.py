af33c0852322dd54a13bf47b76e431897f943278
Paul Prozesky <paulp@ska.ac.za>
2015-09-02 10:34:17 +0200

    TX en/disable funcs for x-engines

--- src/fxcorrelator_xengops.py
+++ src/fxcorrelator_xengops.py
@@ -414,32 +414,52 @@ def xeng_set_acc_len(corr, acc_len=None, vacc_resync=True):
 
 
 def xeng_get_baseline_order(corr):
-        """
-        Return the order of baseline data output by a CASPER correlator X engine.
-        :return:
-        """
-        # TODO - nants vs number of inputs?
-        assert len(corr.fengine_sources)/2 == corr.n_antennas
-        order1 = []
-        order2 = []
-        for ctr1 in range(corr.n_antennas):
-            # print 'ctr1(%d)' % ctr1
-            for ctr2 in range(int(corr.n_antennas/2), -1, -1):
-                temp = (ctr1 - ctr2) % corr.n_antennas
-                # print '\tctr2(%d) temp(%d)' % (ctr2, temp)
-                if ctr1 >= temp:
-                    order1.append((temp, ctr1))
-                else:
-                    order2.append((ctr1, temp))
-        order2 = [order_ for order_ in order2 if order_ not in order1]
-        baseline_order = order1 + order2
-        source_names = []
-        for source in corr.fengine_sources:
-            source_names.append(source.name)
-        rv = []
-        for baseline in baseline_order:
-            rv.append((source_names[baseline[0] * 2],       source_names[baseline[1] * 2]))
-            rv.append((source_names[baseline[0] * 2 + 1],   source_names[baseline[1] * 2 + 1]))
-            rv.append((source_names[baseline[0] * 2],       source_names[baseline[1] * 2 + 1]))
-            rv.append((source_names[baseline[0] * 2 + 1],   source_names[baseline[1] * 2]))
-        return rv
+    """
+    Return the order of baseline data output by a CASPER correlator X engine.
+    :return:
+    """
+    # TODO - nants vs number of inputs?
+    assert len(corr.fengine_sources)/2 == corr.n_antennas
+    order1 = []
+    order2 = []
+    for ctr1 in range(corr.n_antennas):
+        # print 'ctr1(%d)' % ctr1
+        for ctr2 in range(int(corr.n_antennas/2), -1, -1):
+            temp = (ctr1 - ctr2) % corr.n_antennas
+            # print '\tctr2(%d) temp(%d)' % (ctr2, temp)
+            if ctr1 >= temp:
+                order1.append((temp, ctr1))
+            else:
+                order2.append((ctr1, temp))
+    order2 = [order_ for order_ in order2 if order_ not in order1]
+    baseline_order = order1 + order2
+    source_names = []
+    for source in corr.fengine_sources:
+        source_names.append(source.name)
+    rv = []
+    for baseline in baseline_order:
+        rv.append((source_names[baseline[0] * 2],       source_names[baseline[1] * 2]))
+        rv.append((source_names[baseline[0] * 2 + 1],   source_names[baseline[1] * 2 + 1]))
+        rv.append((source_names[baseline[0] * 2],       source_names[baseline[1] * 2 + 1]))
+        rv.append((source_names[baseline[0] * 2 + 1],   source_names[baseline[1] * 2]))
+    return rv
+
+
+def tx_disable(corr):
+    """
+    Start transmission of data products from the x-engines
+    :param corr:
+    :return:
+    """
+    THREADED_FPGA_OP(corr.xhosts, timeout=10,
+                     target_function=(lambda fpga_: fpga_.registers.control.write(gbe_txen=False),))
+
+
+def tx_enable(corr):
+    """
+    Start transmission of data products from the x-engines
+    :param corr:
+    :return:
+    """
+    THREADED_FPGA_OP(corr.xhosts, timeout=10,
+                     target_function=(lambda fpga_: fpga_.registers.control.write(gbe_txen=True),))
