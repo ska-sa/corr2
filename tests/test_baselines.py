11f77566dee78f2eb2330ce4a089bf709d9ce10a
Paul Prozesky <paulp@ska.ac.za>
2015-09-03 14:31:50 +0200

    Edited to mimic testing functionality

--- tests/test_baselines.py
+++ tests/test_baselines.py
@@ -45,6 +45,26 @@ def start_corr():
     print 'Done starting correlator'
     return correlator
 
+
+def match_inputs_to_baselines(baselines, active_inputs):
+    """
+    Which baselines should be non-zero given these active inputs
+    :param baselines:
+    :param active_inputs:
+    :return:
+    """
+    matched_baselines = []
+    for blsctr, baseline in enumerate(baselines):
+        if (baseline[0] in active_inputs) and (baseline[1] in active_inputs):
+            matched_baselines.append(blsctr)
+
+    print baselines
+    print active_inputs
+    print matched_baselines
+
+    return matched_baselines
+
+
 ant_names = []
 for ctr in range(0, 4):
     ant_names.append('ant%i_x' % ctr)
@@ -53,7 +73,7 @@ for ctr in range(0, 4):
 ZEROES_ALLOWED = int(4096*0.7)
 CORR_INIT_LOOPS = 1
 CORR_INIT_RETRIES = 5
-DATA_TO_GET = 100
+DATA_TO_GET = 2
 EXPECTED_TIMESTEP = 8192
 EXPECTED_NACC = 816
 
@@ -79,23 +99,38 @@ try:
             import IPython
             IPython.embed()
         baselines = fxcorrelator_xengops.xeng_get_baseline_order(c)
+        active_inputs = []
+
+        # set all the inputs to zero
+        fxcorrelator_fengops.feng_eq_set(c, new_eq=0)
+
         for ant in ant_names:
             print 'Setting antenna %s to non-zero quantiser' % ant
-            fxcorrelator_fengops.feng_eq_set(c, new_eq=0)
             fxcorrelator_fengops.feng_eq_set(c, source_name=ant, new_eq=300)
-            # print fxcorrelator_fengops.feng_eq_get(c)
-            # find the autocorrelation
-            auto_index = -1
-            for ctr, bls in enumerate(baselines):
-                if bls[0] == ant and bls[1] == ant:
-                    auto_index = ctr
-                    break
-            if auto_index == -1:
+            active_inputs.append(ant)
+            expected_baselines = match_inputs_to_baselines(baselines, active_inputs)
+            if len(expected_baselines) == 0:
                 stop_rx()
                 print 'corr_init_failed: ', corr_init_failed
-                raise RuntimeError('Could not match %s to baseine auto' % ant)
-            print '\tMatched to baseline %i' % auto_index
-            expected_baselines = [auto_index]
+                print baselines
+                print active_inputs
+                print expected_baselines
+                raise RuntimeError('Active inputs not matched in baseline list?!')
+            print 'Active inputs:', active_inputs
+            print 'Matched baseline indices:', expected_baselines
+
+            # # find the autocorrelation
+            # auto_index = -1
+            # for ctr, bls in enumerate(baselines):
+            #     if bls[0] == ant and bls[1] == ant:
+            #         auto_index = ctr
+            #         break
+            # if auto_index == -1:
+            #     stop_rx()
+            #     print 'corr_init_failed: ', corr_init_failed
+            #     raise RuntimeError('Could not match %s to baseine auto' % ant)
+            # print '\tMatched to baseline %i' % auto_index
+            # expected_baselines = [auto_index]
 
             print '\tWaiting for clean dumps'
             rxer.get_clean_dump(5, discard=4)
@@ -142,7 +177,6 @@ try:
                 else:
                     print '\t', cnt, 'ig@%i' % lasttime, 'okay'
                 cnt += 1
-                break
         main_loop_ctr += 1
         runtime_last = time.time() - tic
         runtime_remain = (CORR_INIT_LOOPS - main_loop_ctr) * runtime_last
