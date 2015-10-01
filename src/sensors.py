102a3051a852784588358027e60ea38fc5d872ec
mobrocka <mobrocka@ska.ac.za>
2015-10-01 15:29:08 +0200

    CBFTASKS-300 applied suggested changes

--- src/sensors.py
+++ src/sensors.py
@@ -276,125 +276,125 @@ def setup_sensors(instrument, katcp_server):
 
     # f-engine lru
     for _f in instrument.fhosts:
-        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='%s_feng_lru' % _f.host,
-                        description='F-engine %s LRU okay' % _f.host,
-                        default=True)
+        sensor = Sensor.boolean(name='%s_feng_lru' % _f.host,
+                                description='F-engine %s LRU okay' % _f.host,
+                                default=True)
         katcp_server.add_sensor(sensor)
         instrument._sensors[sensor.name] = sensor
         ioloop.add_callback(_sensor_cb_flru, sensor, executor, _f)
 
     # x-engine lru
     for _x in instrument.xhosts:
-        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='%s_xeng_lru' % _x.host,
-                        description='X-engine %s LRU okay' % _x.host,
-                        default=True)
+        sensor = Sensor.boolean(name='%s_xeng_lru' % _x.host,
+                                description='X-engine %s LRU okay' % _x.host,
+                                default=True)
         katcp_server.add_sensor(sensor)
         instrument._sensors[sensor.name] = sensor
         ioloop.add_callback(_sensor_cb_xlru, sensor, executor, _x)
 
     # f-engine tx counters
     for _f in instrument.fhosts:
-        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='%s_feng_tx' % _f.host,
-                        description='F-engine TX okay - counters incrementing',
-                        default=True)
+        sensor = Sensor.boolean(name='%s_feng_tx' % _f.host,
+                                description='F-engine TX okay - counters incrementing',
+                                default=True)
         katcp_server.add_sensor(sensor)
         instrument._sensors[sensor.name] = sensor
         ioloop.add_callback(_sensor_feng_tx, sensor, executor, _f)
 
     # f-engine rx counters
     for _f in instrument.fhosts:
-        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='%s_feng_rx' % _f.host,
-                        description='F-engine RX okay - counters incrementing',
-                        default=True)
+        sensor = Sensor.boolean(name='%s_feng_rx' % _f.host,
+                                description='F-engine RX okay - counters incrementing',
+                                default=True)
         katcp_server.add_sensor(sensor)
         instrument._sensors[sensor.name] = sensor
         ioloop.add_callback(_sensor_feng_rx, sensor, executor, _f)
 
     # x-engine tx counters
     for _x in instrument.xhosts:
-        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='%s_xeng_tx' % _x.host,
-                        description='X-engine TX okay - counters incrementing',
-                        default=True)
+        sensor = Sensor.boolean(name='%s_xeng_tx' % _x.host,
+                                description='X-engine TX okay - counters incrementing',
+                                default=True)
         katcp_server.add_sensor(sensor)
         instrument._sensors[sensor.name] = sensor
         ioloop.add_callback(_sensor_xeng_tx, sensor, executor, _x)
 
     # x-engine rx counters
     for _x in instrument.xhosts:
-        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='%s_xeng_rx' % _x.host,
-                        description='X-engine RX okay - counters incrementing',
-                        default=True)
+        sensor = Sensor.boolean(name='%s_xeng_rx' % _x.host,
+                                description='X-engine RX okay - counters incrementing',
+                                default=True)
         katcp_server.add_sensor(sensor)
         instrument._sensors[sensor.name] = sensor
         ioloop.add_callback(_sensor_xeng_rx,  sensor, executor, _x)
 
     # x-engine QDR errors
     for _x in instrument.xhosts:
-        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='%s_xeng_qdr' % _x.host,
-                        description='X-engine QDR okay',
-                        default=True)
+        sensor = Sensor.boolean(name='%s_xeng_qdr' % _x.host,
+                                description='X-engine QDR okay',
+                                default=True)
         katcp_server.add_sensor(sensor)
         instrument._sensors[sensor.name] = sensor
         ioloop.add_callback(_xeng_qdr_okay, sensor, executor, _x)
 
     # f-engine QDR errors
     for _f in instrument.fhosts:
-        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='%s_feng_qdr' % _f.host,
-                        description='F-engine QDR okay',
-                        default=True)
+        sensor = Sensor.boolean(name='%s_feng_qdr' % _f.host,
+                                description='F-engine QDR okay',
+                                default=True)
         katcp_server.add_sensor(sensor)
         instrument._sensors[sensor.name] = sensor
         ioloop.add_callback(_feng_qdr_okay, sensor, executor, _f)
 
     # x-engine PHY counters
     for _x in instrument.xhosts:
-        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='%s_xeng_phy' % _x.host,
-                        description='X-engine PHY okay',
-                        default=True)
+        sensor = Sensor.boolean(name='%s_xeng_phy' % _x.host,
+                                description='X-engine PHY okay',
+                                default=True)
         katcp_server.add_sensor(sensor)
         instrument._sensors[sensor.name] = sensor
         ioloop.add_callback(_sensor_xeng_phy, sensor, executor, _x)
 
     # f-engine PHY counters
     for _f in instrument.fhosts:
-        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='%s_feng_phy' % _f.host,
-                        description='F-engine PHY okay',
-                        default=True)
+        sensor = Sensor.boolean(name='%s_feng_phy' % _f.host,
+                                description='F-engine PHY okay',
+                                default=True)
         katcp_server.add_sensor(sensor)
         instrument._sensors[sensor.name] = sensor
         ioloop.add_callback(_sensor_feng_phy, sensor, executor, _f)
 
     # f-engine PFB counters
     for _f in instrument.fhosts:
-        sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='%s_feng_pfb' % _f.host,
-                        description='F-engine PFB okay',
-                        default=True)
+        sensor = Sensor.boolean(name='%s_feng_pfb' % _f.host,
+                                description='F-engine PFB okay',
+                                default=True)
         katcp_server.add_sensor(sensor)
         instrument._sensors[sensor.name] = sensor
         ioloop.add_callback(_feng_pfb_okay, sensor, executor, _f)
 
     # f-engine comms rx
-    fengops = corr2.fxcorrelator_fengops.FEngineOperations(instrument)
-    sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='fhosts_check_rx',
-                    description='F-hosts rx okay',
-                    default=True)
+    fengops = instrument.fops
+    sensor = Sensor.boolean(name='fhosts_check_rx',
+                            description='F-hosts rx okay',
+                            default=True)
     katcp_server.add_sensor(sensor)
     instrument._sensors[sensor.name] = sensor
     ioloop.add_callback(_fhost_check_rx, sensor, executor, fengops)
 
     # # f-engine comms tx
-    sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='fhosts_check_tx',
-                    description='F-hosts tx okay',
-                    default=True)
+    sensor = Sensor.boolean(name='fhosts_check_tx',
+                            description='F-hosts tx okay',
+                            default=True)
     katcp_server.add_sensor(sensor)
     instrument._sensors[sensor.name] = sensor
     ioloop.add_callback(_fhost_check_tx, sensor, executor, fengops)
 
     # x-engine comms rx
-    xengops = corr2.fxcorrelator_xengops.XEngineOperations(instrument)
-    sensor = Sensor(sensor_type=Sensor.BOOLEAN, name='xhosts_check_rx',
-                    description='X-hosts rx okay',
-                    default=True)
+    xengops = instrument.xops
+    sensor = Sensor.boolean(name='xhosts_check_rx',
+                            description='X-hosts rx okay',
+                            default=True)
     katcp_server.add_sensor(sensor)
     instrument._sensors[sensor.name] = sensor
     ioloop.add_callback(_xhost_check_rx, sensor, executor, xengops)
