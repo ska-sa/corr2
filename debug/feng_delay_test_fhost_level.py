54ff2898e1d54026f77756dd75076723ad0f43e8
andrew <andrew@ska.ac.za>
2015-10-23 17:34:26 +0200

    delay tracking debug script changes

--- debug/feng_delay_test_fhost_level.py
+++ debug/feng_delay_test_fhost_level.py
@@ -8,24 +8,31 @@ Created on Tuesday Oct  6 2015
 
 @author: andrew
 """
-import corr2, os, time
+import corr2, os, time, logging
 import matplotlib.pyplot as plt
+
+logging.basicConfig(level=logging.INFO)
 
 ant_name = 'ant0_y'
 sample_rate = 1712*(10**6)
 shift_bits = 23
 sleep_time = 2
+ld_time = 1
+wait_time = 2
+
 offset = 1
+load_check_wait_time = None
+load_check = False
 
 #delay
-delay_val = 0
+delay_val = 1
 
 #delta delay
 delta_delay_val_samples = 0
 delta_delay_val = float(delta_delay_val_samples) / (float(sleep_time) * sample_rate)
 
 #phase
-phase_val = 0.5
+phase_val = 0
 
 #delta phase
 delta_phase_val_samples = 0
@@ -33,6 +40,7 @@ delta_phase_val = float(delta_phase_val_samples) / (float(sleep_time) * sample_r
 
 c = corr2.fxcorrelator.FxCorrelator('rts wbc', config_source=os.environ['CORR2INI'])
 c.initialise(qdr_cal=False,program=False)
+c.est_sync_epoch()
 f = c.fhosts[0]
 
 # set up tvg
@@ -42,15 +50,29 @@ f.registers.control.write(tvg_adc=1)
 # capture data before setting up delays
 x_0 = f.snapshots.snap_quant1_ss.read(man_valid=False, man_trig=False)['data']
 
-# set delay
-f.write_delay(ant_name, delay_val, delta_delay_val, phase_val, delta_phase_val)
-#c.fops.set_delay(ant_name, delay_val_seconds, delta_delay_val_seconds_per_second, phase_val_radians, delta_phase_val_radians_per_sleep_time)
+mcnt = None
+if ld_time is not None:
+    mcnt = c.mcnt_from_time(time.time() + float(ld_time))
 
-# capture data just after setting up delaysa
+# set delay
+vals = f.write_delay(ant_name, delay_val, delta_delay_val, phase_val, delta_phase_val, mcnt, load_check_wait_time, load_check)
+
+# capture data just after setting up delays
 x_1 = f.snapshots.snap_quant1_ss.read(man_valid=False, man_trig=False)['data']
 
+print('delay after = %d'%vals['act_delay'])
+print('delta delay after = %f'%vals['act_delta_delay'])
+print('phase offset after = %f'%vals['act_phase_offset'])
+print('delta phase offset after = %f'%vals['act_delta_phase_offset'])
+
+status_before = f.registers.tl_cd1_status.read()['data']
+print('counters before: arm => %d load => %d'%(status_before['arm_count'], status_before['arm_count']))
+
 # sleep 
-time.sleep(sleep_time)
+time.sleep(sleep_time+wait_time)
+
+status_after = f.registers.tl_cd1_status.read()['data']
+print('counters after: arm => %d load => %d'%(status_after['arm_count'], status_after['arm_count']))
 
 # capture data after
 x_2 = f.snapshots.snap_quant1_ss.read(man_valid=False, man_trig=False)['data']
