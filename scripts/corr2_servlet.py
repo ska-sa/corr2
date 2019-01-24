ec529ac46c1bc46cf87865edfd6157a7633b6a82
Amish <apatel@ska.ac.za>
2019-01-24 14:02:02 +0200

    Updated corr2_servlet to log details of bitstreams used to create correlators. This includes git info of mkat_fpga and mlib_devel used to create the fpg-file.

--- scripts/corr2_servlet.py
+++ scripts/corr2_servlet.py
@@ -22,6 +22,8 @@ from corr2.utils import parse_ini_file
 
 from corr2 import corr_monitoring_loop as corr_mon_loop
 
+from casperfpga.utils import get_git_info_from_fpg
+
 
 class Corr2Server(katcp.DeviceServer):
 
@@ -114,14 +116,42 @@ class Corr2Server(katcp.DeviceServer):
             except AssertionError as ex:
                 self._log_excep(ex, "Logging dir {} does not exist".format(_default_log_dir))
 
-            # While we have the config-file parsed
+            # While we have the config-file parsed:
             # - To be used when initialising log-levels of instrument groups
-            self.log_leveld = config_file_dict.get('log-level', None)
+            self.log_level_dict = config_file_dict.get('log-level', None)
 
+            # - Get the bitstream filenames and corresponding git-info(s)
+            fpg_files = []
+            for key_1, value_1 in config_file_dict.items():
+                for key_2, value_2 in value_1.items():
+                    if key_2 == 'bitstream':
+                        fpg_files.append(value_2)
+            
+            # - Now, get the git-info
+            #   - Dictionary: Key = bitstream location
+            #                 Value = dictionary of git-info
+            bitstream_dict = {}
+            for filename in fpg_files:
+                bitstream_dict[filename] = get_git_info_from_fpg(filename)
+
+            # Unfortunately can only log against the instrument.logger 
+            # AFTER the instrument has been created
+            
             self.instrument = fxcorrelator.FxCorrelator(iname, config_source=config_file,
                               getLogger=getKatcpLogger, mass_inform_func=self.mass_inform,
                               log_filename=self.log_filename, log_file_dir=self.log_file_dir)
             self._created = True
+            
+            # Better to stitch a log-string together
+            # than to log in a for-loop
+            bitstream_info_str = ''
+            for fpg_filename, git_info in bitstream_dict.items():
+                bitstream_info_str += '{}\n'.format(fpg_filename)
+                for git_repo, git_version in git_info.items():
+                    bitstream_info_str += '\t {} \n \t {} \n\n'.format(git_repo, git_version)
+            
+            # Log the bitstream info
+            self.instrument.logger.info(bitstream_info_str)
 
             # Function created to reassign all non-conforming log-handlers
             loggers_changed = reassign_log_handlers(mass_inform_func=self.mass_inform,
@@ -132,6 +162,7 @@ class Corr2Server(katcp.DeviceServer):
             return 'ok',
         except Exception as ex:
             return self._log_excep(ex, 'Failed to create instrument.')
+
 
     def setup_sensors(self):
         """
@@ -195,11 +226,11 @@ class Corr2Server(katcp.DeviceServer):
 
             # Last thing to do, check if any default log-levels are specified in the config_file
             # - Change the corresponding group's log-level accordingly
-            if self.log_leveld is None:
+            if self.log_level_dict is None:
                 # All is well
                 return 'ok',
             # else: More work to do!
-            for logger_group_name, log_level in self.log_leveld.items():
+            for logger_group_name, log_level in self.log_level_dict.items():
                 result, return_msg = servlet_log_level_request(corr_obj=self.instrument,
                                                                log_level=log_level,
                                                                logger_group_name=logger_group_name)
