238a13e7fa0401ea493852435f3eaa39b4b8314b
Andrew Martens <andrew@ska.ac.za>
2022-08-10 17:31:35 +0200

    <fix> range needs integer values
    Potential vacc timestamps now generated correctly.

--- src/fxcorrelator_xengops.py
+++ src/fxcorrelator_xengops.py
@@ -331,19 +331,19 @@ class XEngineOperations(object):
         :return: True or False
         """ 
         t0 = time.time()
-        rv=THREADED_FPGA_FUNC(self.hosts, timeout=self.timeout,
+        rv = THREADED_FPGA_FUNC(self.hosts, timeout=self.timeout,
                                   target_function='get_vacc_timestamps')
         t_delta = time.time() - t0
+        
         acc_len = self.vacc_acc_len
-        t_delta = self.acc_time_from_len(acc_len)
         timestamp=rv[rv.keys()[0]][0]['timestamp']
         
         import numpy as np
         accs = np.ceil(t_delta/self.acc_time_from_len(acc_len))
 
         #timestamps possible based on how long it took to read all registers
-        timestamps = range(timestamp-(accs*acc_len), timestamp+((accs+1)*acc_len), acc_len)
-        self.logger.error('timestamps - %s' %(disp(accs)))      
+        timestamps = range(int(timestamp-(accs*acc_len)), int(timestamp+((accs+1)*acc_len)), int(acc_len))
+        #self.logger.error('timestamps - %s' %(str(accs)))      
  
         #check that they're all in sync; 
         #allow for reading registers at acc boundary:
@@ -352,7 +352,7 @@ class XEngineOperations(object):
             for vacc in rv[hostname]:
                 ts = vacc['timestamp']
                 if (timestamps.count(ts) == 0):
-                    self.logger.error('timestamp %d not in range %s' %(ts, disp(timestamps))) 
+                    self.logger.error('vacc timestamp %d not in valid range %s' %(ts, str(timestamps))) 
                     sync=False
         return sync
 
